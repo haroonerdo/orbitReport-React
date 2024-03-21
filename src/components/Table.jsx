1a3e06ce867297b841ed469f08771bf481dba1d5
Ayse Erdogan <aerdogan@AErdogan-Fyler.local>
2024-03-21 15:41:10 -0500

    assignment 6 first commit

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,28 @@
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satallite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {sat.map((data, id) => {
+          return (
+            <tr key={{ id }}>
+              <td>{data.name}</td>
+              <td>{data.type}</td>
+              <td>{data.launchDate}</td>
+              <td>{`${data.operational === true ? "active" : "inactive"} `}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
