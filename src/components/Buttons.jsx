c1facc310dd9cc6a9ede5541078140dd644cbecb
Ayse Erdogan <aerdogan@AErdogan-Fyler.local>
2024-03-21 20:38:21 -0500

    2. commit a little change

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,16 +1,19 @@
 import satData from "./satData";
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
-  const typeButtons = displaySats.map((sat, id) => {
-    return (
-      <button onClick={() => filterByType(sat)} key={id}>
-        {sat} Orbit
-      </button>
-    );
-  });
+  // const typeButtons = displaySats.map((sat, id) => {
+  //   return (
+  //     <button onClick={() => filterByType(sat)} key={id}>
+  //       {sat} Orbit
+  //     </button>
+  //   );
+  //});
   return (
     <div className="flex-container">
-      {typeButtons}
+      
+      <button onClick={() => setSat(satData)}>Low Orbits</button>
+      <button onClick={() => setSat(satData)}>Medium Orbits</button>
+      <button onClick={() => setSat(satData)}>High Orbits</button>
       <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
